10804d7f5bc0e326bd74f8e3230415d0235e635b
Jose Ulisses <joseulisses03@gmail.com>
2024-07-26 13:37:14 -0300

    adding swagger docs for the users CRUD

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import "reflect-metadata";
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Nikitchen API")
+    .setDescription("API documentation for the backend of the Nikitchen blog")
+    .setVersion("0.1")
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
